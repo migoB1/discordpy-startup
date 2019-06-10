940503f4cc37361f2fcad81acd823a62c72bae0a
migoB1 <46myago@gmail.com>
2019-06-10 23:12:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
+import random
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -10,10 +11,12 @@ token = os.environ['DISCORD_BOT_TOKEN']
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
 
+goroku_a=["nuuu","kyuubee","tueee","iyaaa"]
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('やぁ')
+    aaa=random.randint(0,3)
+    await ctx.send(goroku_a[aaa])
 
 
 bot.run(token)
